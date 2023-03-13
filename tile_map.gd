f3bf83201288c7c1079c6f63ae2429a871339f55
Zishan Rahman <zishanfrahman@outlook.com>
2023-03-13 18:03:18 +0000

    Update tile_map.gd so that paint method for LSystem is called on main TileMap scene now

--- tile_map.gd
+++ tile_map.gd
@@ -1,5 +1,7 @@
 extends TileMap
 
+@onready var l_system: LSystem = $LSystem
+
 var x_tile_range: int = ProjectSettings.get_setting("display/window/size/viewport_width") / tile_set.tile_size.x
 var y_tile_range: int = ProjectSettings.get_setting("display/window/size/viewport_height") / tile_set.tile_size.y
 
@@ -9,14 +11,13 @@ const GRASS_2: Vector2i = Vector2i(5, 1)
 func pick_grass_tile() -> Vector2i:
 	return [GRASS_1, GRASS_2].pick_random()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	randomize()
+func cover_map_with_grass() -> void:
 	for x in range(-50, x_tile_range + 50):
 		for y in range(-50, y_tile_range + 50):
 			set_cell(0, Vector2i(x, y), 0, pick_grass_tile())
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta) -> void:
-	pass
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	randomize()
+	cover_map_with_grass()
+	l_system.paint()
